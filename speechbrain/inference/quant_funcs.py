8143987aa565ab4eb0c763ed85c0180ac50f6374
personusinggit <kdww2@cam.ac.uk>
2024-03-20 14:10:30 +0000

    take 1 of speed avg

--- speechbrain/inference/quant_funcs.py
+++ speechbrain/inference/quant_funcs.py
@@ -1,3 +1,22 @@
+from speechbrain.inference import ASR_Quant_D
 def speed(z):
     print(z)
+    file.write(str(z)+"\n")
+    print("1 loop")
     return
+
+def run_speed_measurement(model,n,audio):
+    with open("numbers.txt","a") as file:
+        for i in range(n):
+            print(i)
+            #call out boi
+            model.transcribe_file(audio)
+    file.close()
+    with open("numbers.txt","r")as file:
+        lines = file.readlines()
+    file.close()
+    numbers_from_file = [int(line.strip()) for line in lines]
+    total_sum = sum(numbers_from_file)
+    avg=total_sum/n
+    return avg
+    
